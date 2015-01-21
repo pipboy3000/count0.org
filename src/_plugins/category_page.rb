fd28ca53639e22ded515a788a5274ba734a96e35
Asai Masami <me@asaimasami.com>
2015-01-22 03:48:25 +0900

    fix category_page plugin

--- src/_plugins/category_page.rb
+++ src/_plugins/category_page.rb
@@ -7,16 +7,17 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.read_yaml(File.join(@base, '_layouts'), 'category_index.html')
 
       self.data['category'] = category
       self.data['title'] = page_title(category)
 
       self.data['posts'] = []
-      post_base = File.join(base, '_posts')
+      post_base = File.join(@base, '_posts')
       Dir.glob(post_base + "/*").each do |post_file|
-        post = Post.new(site, base, '', File.basename(post_file))
+        post = Post.new(@site, @base, '', File.basename(post_file))
         if post.categories.include? category
+          post.content = post.transform.to_s
           self.data['posts'] << post
         end
       end
@@ -25,7 +26,7 @@ module Jekyll
     end
 
     def page_title(category)
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      category_title_prefix = @site.config['category_title_prefix'] || 'Category: '
      "#{category_title_prefix}#{category}" 
     end
   end
@@ -35,9 +36,10 @@ module Jekyll
     
     def generate(site)
       if site.layouts.key? 'category_index'
-        dir = site.config['category_dir'] || 'categories'
+        base_dir = site.config['category_dir'] || 'categories'
         site.categories.keys.each do |category|
-          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+          cat_dir = File.join(base_dir, category) 
+          site.pages << CategoryPage.new(site, site.source, cat_dir, category)
         end
       end
     end
