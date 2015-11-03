f9957fd347feb499234c11b49087c2cab5159812
Asai Masami <shakeforme@gmail.com>
2015-11-03 14:56:24 +0900

    Category and Archive page plugin output JSON
    * drop paginate in Archive page plugin

--- src/html/_plugins/archive_page.rb
+++ src/html/_plugins/archive_page.rb
@@ -1,19 +1,25 @@
-require 'pry'
-
 module Jekyll
   class ArchivePage < Page
-    def initialize(site, base, dir)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
+    def initialize(site, base, dir, name = nil, layout = nil)
+      @site   = site
+      @base   = base
+      @dir    = dir
+      @name   = name || 'index.html'
+      @layout = layout || 'archive_index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(@base, '_layouts'), 'archive_index.html')
+      self.read_yaml(File.join(@base, '_layouts'), @layout)
       self.data['title'] = 'Archives'
       self.data['archives'] = []
-      self.data['archives'] = site.posts.docs.group_by { |post| post.date.year }
+      posts = @site.posts.docs.reverse
+      self.data['archives'] = posts.group_by { |post| post.date.year }
     end        
+  end
+
+  class ArchiveJson < ArchivePage
+    def initialize(site, base, dir)
+      super(site, base, dir, 'index.json', 'archive_index.json')
+    end
   end
 
   class ArchivePageGenerator < Generator
@@ -23,6 +29,7 @@ module Jekyll
       if site.layouts.key? 'archive_index'
         dir = site.config['archive_dir'] || 'archives'
         site.pages << ArchivePage.new(site, site.source, dir)
+        site.pages << ArchiveJson.new(site, site.source, dir)
       end
     end
   end
--- src/html/_plugins/category_page.rb
+++ src/html/_plugins/category_page.rb
@@ -1,19 +1,21 @@
 module Jekyll
   class CategoryPage < Page
-    def initialize(site, base, dir, category)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
+    def initialize(site, base, dir, category, name = nil, layout = nil)
+      @site     = site
+      @base     = base
+      @dir      = dir
+      @category = category
+      @name     = name || 'index.html'
+      @layout   = layout || 'category_index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(@base, '_layouts'), 'category_index.html')
-      self.data['category'] = category
-      self.data['title'] = page_title(category)
+      self.read_yaml(File.join(@base, '_layouts'), @layout)
+      self.data['category'] = @category
+      self.data['title'] = page_title(@category)
       self.data['posts'] = []
 
-      site.posts.docs.each do |post|
-        self.data['posts'] << post if post.data["categories"].include? category
+      @site.posts.docs.each do |post|
+        self.data['posts'] << post if post.data["categories"].include? @category
       end
 
       self.data['posts'].reverse!
@@ -22,6 +24,12 @@ module Jekyll
     def page_title(category)
       category_title_prefix = @site.config['category_title_prefix'] || 'Category: '
      "#{category_title_prefix}#{category}" 
+    end
+  end
+
+  class CategoryJson < CategoryPage
+    def initialize(site, base, dir, category)
+      super(site, base, dir, category, 'index.json', 'category_index.json')
     end
   end
 
@@ -34,6 +42,7 @@ module Jekyll
         site.categories.keys.each do |category|
           cat_dir = File.join(dir, category) 
           site.pages << CategoryPage.new(site, site.source, cat_dir, category)
+          site.pages << CategoryJson.new(site, site.source, cat_dir, category)
         end
       end
     end
