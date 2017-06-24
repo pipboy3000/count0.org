5c66f4093cfd8dece937bf6c0039a46f0ecb6588
Asai Masami <shakeforme@gmail.com>
2017-06-25 01:46:56 +0900

    babiliで圧縮

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const moduleImporter = require('sass-module-importer');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BabiliPlugin = require('babili-webpack-plugin')
 
 module.exports = {
   module: {
@@ -79,3 +80,15 @@ module.exports = {
   },
   devtool: 'source-map'
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
+    }),
+    new BabiliPlugin(),
+    new webpack.LoaderOptionsPlugin({ minimize: true })
+  ])
+}
