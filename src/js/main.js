81f7960bdb34132383d2cf3c121fcfc25781f532
Asai Masami <shakeforme@gmail.com>
2020-09-07 23:12:28 +0900

    atomを自前に戻し、カテゴリーページを廃止

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,5 @@
-import Vue from 'vue';
 import mobileHeader from './mobileHeader'
 import '../scss/style.scss';
 
 // mobile header transform 
 mobileHeader()
-
-// Vue
-import RelatedPosts from './components/RelatedPosts.vue'
-
-new Vue({
-  el: document.getElementById('related-posts'),
-  components: {
-    RelatedPosts
-  },
-  template: '<RelatedPosts></RelatedPosts>'
-});
